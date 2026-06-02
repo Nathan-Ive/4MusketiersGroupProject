a40192f6845166644411912bdb3de8e3543dfba3
Lm-Pjunior <julian.vlaming@student.rocvf.nl>
2026-06-02 11:59:30 +0200

    Class creation
    parent class created
    now I just need to study ;-;

---
+++ RunningSlim/Assets/Scripts/Foods/FoodInheritance.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FoodInheritance : MonoBehaviour //it's a parent class
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
