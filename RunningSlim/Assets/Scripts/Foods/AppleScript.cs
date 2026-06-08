477cd49a568d8e7b83903195aef7563674c0cb92
Lm-Pjunior <julian.vlaming@student.rocvf.nl>
2026-06-08 13:07:12 +0200

    FoodFixing
    -removing unnecesary class stamina foods
    -condensing foodinheritance further

--- RunningSlim/Assets/Scripts/Foods/AppleScript.cs
+++ RunningSlim/Assets/Scripts/Foods/AppleScript.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 
-public class AppleScript : StaminaFood
+abstract class AppleScript : FoodInheritance
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+
+    override public void Eating() 
+    {
+        //_getStats.Stamina
+    }
 }
--- RunningSlim/Assets/Scripts/Foods/FoodInheritance.cs
+++ RunningSlim/Assets/Scripts/Foods/FoodInheritance.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 
-public class FoodInheritance : MonoBehaviour //it's a parent class
+abstract class FoodInheritance : MonoBehaviour //it's a parent class
 {
     [SerializeField] private string _name;
     [SerializeField] private string _desc;
     //props best to use get set 
+    protected StatsV1 _getStats = new StatsV1();
     void Start()
     {
-        
+        _getStats = FindAnyObjectByType<StatsV1>();
     }
-    protected void Eating()
-    {
+    abstract public void Eating();
+    
         //protected means that only the parent and child classes have acces to this
-    }
+    
 
     // Update is called once per frame
     public void OnMouseDown()
@@ -21,6 +22,7 @@ public class FoodInheritance : MonoBehaviour //it's a parent class
         Eating();
         Destroy(gameObject);
     }
+
    
     //a UI that's seprate from the food prefab that gets the component and displays the food name and description.
 }
